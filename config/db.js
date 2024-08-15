dee1df0782f63e9024fca07494be190ab0820381
codewithhariom <om.hari99901@gmail.com>
2024-08-15 12:00:33 +0530

    db connection changes

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose= require("mongoose");
-const DB_URl=process.env.DB_URl;
+const DB_URl=process.env.DB_URl || "mongodb+srv://myapp:Test123@cluster0.qynl1.mongodb.net/";
 module.exports= {
     connectMongoDb: ()=>{
         mongoose.connect(DB_URl).then(res=>{
